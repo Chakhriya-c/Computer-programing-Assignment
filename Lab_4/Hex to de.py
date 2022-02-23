1b888a4215044d8920a6f9503bc03c63907038ae
Chakhriya <chakhriyaj@gmail.com>
2022-02-23 10:22:04 +0700

    Upload Lab 4
    - Hex to de
    - Multiply element in list

---
+++ Lab_4/Hex to de.py
@@ -0,0 +1,10 @@
+print("Insert your hexadecimal")
+myhex = input()
+
+def hexadecimal_to_decimal(myhex):
+    decimal = int(myhex, 16)
+    str(decimal)
+    print (decimal)
+
+hexadecimal_to_decimal(myhex)
+
